5690429573bc793f616cf3e8a2e62b41c8d50055
kioL12 <james2002gs@gmail.com>
2022-11-30 23:17:20 +0100

    xml Creados

---
+++ EvaluacionT1_JGS/app/src/main/java/com/example/evaluaciont1_jgs/ConsultaActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaluaciont1_jgs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta);
+    }
+}
---
+++ EvaluacionT1_JGS/app/src/main/java/com/example/evaluaciont1_jgs/SeleccionActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaluaciont1_jgs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeleccionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion);
+    }
+}
